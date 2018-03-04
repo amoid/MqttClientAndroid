a76d932dd93d60c6a9cab78e9b050381bc5c9b96
MarkZhu <markz@onemoretek.com>
2018-03-04 22:08:34 +0800

    Supported shiboqi

--- app/src/main/java/com/xugaoxiang/mqttclientandroid/MainActivity.java
+++ app/src/main/java/com/xugaoxiang/mqttclientandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.xugaoxiang.mqttclientandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -87,6 +88,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }).start();
 
+            }
+        });
+
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                startActivity(intent);
             }
         });
 
