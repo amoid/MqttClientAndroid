34381ca201272422af63299b837320cda87e1705
Zewan Zhu <zewanz@qti.qualcomm.com>
2018-03-05 21:14:48 +0800

    Added code to get make clientid from mac of wlan0 or serial no

--- app/src/main/java/com/xugaoxiang/mqttclientandroid/MainActivity.java
+++ app/src/main/java/com/xugaoxiang/mqttclientandroid/MainActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.xugaoxiang.mqttclientandroid.mqtt.MqttManager;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.greenrobot.eventbus.Subscribe;
+
+import java.lang.reflect.Method;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String password = "password";
 
-    private String clientId = "901000";
+    private String clientId = null;
 
     private static final String TOPIC = "shopping";
     private static final String TOPIC_MESSAGE = "What a nice day! Go shopping with me?";
@@ -32,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clientId = getAddressMacByIf();
+
+        if (clientId == null) {
+            clientId = getSerialNum();
+        }
+
+        //Toast.makeText(getApplicationContext(), "Client id is " + clientId, Toast.LENGTH_LONG).show();
 
         //连接
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
@@ -125,5 +140,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    public String getAddressMacByIf () {
+        try  {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                //Toast.makeText(this, nif.getName() + nif.getHardwareAddress(), Toast.LENGTH_LONG).show();
+                if (nif.getName().equalsIgnoreCase("wlan0")) {
+                    byte[] macBytes = nif.getHardwareAddress();
+                    if (macBytes == null) {
+                        return "";
+                    }
+                    StringBuilder res1 = new StringBuilder();
+                    for (byte b : macBytes) {
+                        res1.append(String.format("%02x", b));
+                    }
+                    return res1.toString();
+                }
+            }
+        } catch (Exception e) {
+            Log.e("MobileAccess", "Error lecture propritiete Address MAC ");
+        }
+        return null;
+    }
+
+    public String getSerialNum() {
+        String serialNum = null;
+
+        try {
+            Class <?> c = Class.forName("android.os.SystemProperties");
+            Method get = c.getMethod("get", String.class);
+            serialNum = (String)get.invoke(c, "ro.serialno");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Toast.makeText(this, "serialNo is " + serialNum, Toast.LENGTH_LONG).show();
+        return serialNum;
     }
 }
